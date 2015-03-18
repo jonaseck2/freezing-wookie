42574a0da448539f254c2b3d379be1b115a6e993
Jonas Eckerström <jonaseck@gmail.com>
2015-03-18 13:31:03 +0100

    added deploy as service script

---
+++ deploy.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+if [ ! -f ./github_secret.env ]; then
+	echo "github_secret.env is missing or empty"
+	exit 1
+fi
+source ./github_secret.env
+if [ -z $GITHUB_SECRET ]; then
+	echo "github_secret.env should set the environment variable GITHUB_SECRET=<your secret here>"
+	exit 1
+fi
+
+#installing docker if missing
+if [ ! -x /usr/bin/docker ]; then
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+	sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+	sudo apt-get update
+	sudo apt-get -qqy install lxc-docker
+	sudo usermod -a -G docker `id -g -n`  # för att slippa köra docker med sudo
+fi
+	
+if [ ! -f /etc/init/mongo.conf ]; then
+	sudo docker pull dockerfile/mongodb
+	sudo mkdir -p /var/log/mongo
+	sudo sh -c "echo '
+description \"A job for running a MongoDB docker container\"
+author \"Joakim Carlstein\"
+
+start on filesystem on runlevel [2345]
+stop on shutdown
+
+exec docker run --rm -p 27017:27017 -v /var/mongo/db:/data/db dockerfile/mongodb >> /var/log/mongo/mongo.log
+respawn' > /etc/init/mongo.conf"
+
+	sudo init-checkconf /etc/init/mongo.conf
+fi
+sudo service mongo start
+
+if [ ! -f /etc/init/katalog.conf ]; then
+	sudo docker pull joakimbeng/katalog
+
+	#workaround to copy the nginx.mustache file from the container that gets overwritten when sharing the tpl volume
+	mkdir -p /var/katalog/
+	rm -f tmp.cid
+	docker run --privileged -d --cidfile tmp.cid -v /var/run/docker.sock:/var/run/docker.sock joakimbeng/katalog
+	sudo docker cp `cat tmp.cid`:/app/tpl/ /var/katalog/
+	docker kill `cat tmp.cid`
+	rm -f tmp.cid
+
+	sudo mkdir -p /var/log/katalog/
+
+	sudo sh -c "echo '
+description \"A job for running a Katalog docker container\"
+author \"Joakim Carlstein\"
+
+start on filesystem on runlevel [2345]
+stop on shutdown
+
+exec docker run --rm --privileged -p 5005:5005 -v /var/run/docker.sock:/var/run/docker.sock -v /var/katalog/tpl:/app/tpl -v /var/katalog/data:/app/data -v /var/katalog/nginx:/app/nginx joakimbeng/katalog >> /var/log/katalog/katalog.log
+respawn' > /etc/init/katalog.conf"
+
+	sudo init-checkconf /etc/init/katalog.conf
+fi
+sudo service katalog start
+
+if [ ! -f /etc/init/nginx-proxy.conf ]; then
+	sudo docker pull joakimbeng/nginx-site-watcher
+	sudo mkdir -p /var/log/nginx
+	sudo touch /var/log/nginx/nginx.log
+	sudo sh -c "echo '
+description \"A job for running a Nginx-site-watcher docker container\"
+author \"Joakim Carlstein\"
+
+start on filesystem on runlevel [2345]
+stop on shutdown
+
+exec docker run --rm -p 80:80 -v /etc/localtime:/etc/localtime:ro -v /var/katalog/nginx:/etc/nginx/sites-enabled -v /var/log/nginx:/var/log/nginx joakimbeng/nginx-site-watcher >> /var/log/nginx/nginx.log
+respawn' > /etc/init/nginx-proxy.conf"
+#remove 80:80 when adding oauth
+
+	sudo init-checkconf /etc/init/nginx-proxy.conf
+fi
+sudo service nginx-proxy start
+
+#api
+if [ `docker images | grep laughing-batman | wc -l` -le 0 ]; then
+	docker build -t softhouse/laughing-batman https://github.com/Softhouse/laughing-batman.git
+fi
+if [ `docker ps | grep laughing-batman | wc -l` -le 0  ]; then
+	docker run -d --name softhouse_laughing-batman_1 -e GITHUB_SECRET=$GITHUB_SECRET -e KATALOG_VHOSTS=default/api -e MONGO_HOST=172.17.42.1 softhouse/laughing-batman
+fi
+
+#builder 
+if [ `docker images | grep flaming-computing-machine | wc -l` -le 0 ];then  
+	docker build -t softhouse/flaming-computing-machine https://github.com/Softhouse/flaming-computing-machine.git
+fi
+if [ `docker ps | grep flaming-computing-machine | wc -l` -le 0  ]; then
+	docker run -d --name softhouse_flaming-computing-machine -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker -e GITHUB_SECRET=$GITHUB_SECRET -e MONGO_HOST=172.17.42.1 softhouse/flaming-computing-machine
+fi
