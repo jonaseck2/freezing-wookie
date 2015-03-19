58426d6195ce9f8bd9f29a85e3d5bc721efe1633
Jonas Eckerström <jonaseck@gmail.com>
2015-03-19 19:39:02 +0100

    template env files are now created if missing

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,39 @@
 #!/usr/bin/env bash
 
+#check prerequisites
 if [ ! -f ./github_secret.env ]; then
-	echo "github_secret.env is missing or empty"
-	exit 1
+	echo "creating default github_secret.env file"
+	echo "This secret should match the configuration set in your github hooks"
+	echo "GITHUB_SECRET=default_secret" > github_secret.env
 fi
 source ./github_secret.env
 if [ -z $GITHUB_SECRET ]; then
-	echo "github_secret.env should set the environment variable GITHUB_SECRET=<your secret here>"
+	echo "Invalid github_secret.env file, should set environment variable"
+	echo "GITHUB_SECRET=<your secret here>"
 	exit 1
 fi
 
-#installing docker if missing
+if [ ! -f ./google_auth_proxy.env ]; then
+	echo "creating default google_auth_proxy.env file"
+	echo "For instructions how to obtain client id and client secret, see"
+	echo "https://github.com/bitly/google_auth_proxy#oauth-configuration"
+	echo "GOOGLE_AUTH_PROXY_CLIENT_ID=default_client_id\nGOOGLE_AUTH_PROXY_CLIENT_SECRET=default_client_secret" > google_auth_proxy.env
+fi
+source ./google_auth_proxy.env
+if [ -z $GOOGLE_AUTH_PROXY_CLIENT_ID || -z $GOOGLE_AUTH_PROXY_CLIENT_SECRET ]; then
+	echo "Invalid google_auth_proxy.env file, should set the environment variables"
+	echo "GOOGLE_AUTH_PROXY_CLIENT_ID=<your client id here>"
+	echo "GOOGLE_AUTH_PROXY_CLIENT_SECRET=<your client secret here>"
+	exit 1
+fi
+
+#install docker if missing
 if [ ! -x /usr/bin/docker ]; then
 	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 	sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
 	sudo apt-get update
 	sudo apt-get -qqy install lxc-docker
-	sudo usermod -a -G docker `id -g -n`  # för att slippa köra docker med sudo
+	sudo usermod -a -G docker `id -g -n`  # emable running docker without sudo
 fi
 	
 if [ ! -f /etc/init/mongo.conf ]; then
