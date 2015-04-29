9a2e6fc961f29782eb67497066a264563c187311
Jonas Eckerström <jonas.eckerstrom@softhouse.se>
2015-04-29 20:11:12 +0200

    downgraded to docker 1.5, required by katalog

--- docker-compose.sh
+++ docker-compose.sh
@@ -7,6 +7,13 @@ sudo apt-get update && sudo apt-get install -qqy curl
 #installing docker
 if [ ! -x /usr/bin/docker ]; then
 	curl -s https://get.docker.com/ | sh
+
+	#downgrade
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+	sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
+	sudo apt-get update
+	sudo apt-get -qqy install lxc-docker-1.5.0
+
 	sudo usermod -a -G docker `id -g -n` # requires relogin
 fi
 
