9febc6db5395cef688f3930c058e64764ab77f80
Jonas Eckerström <jonaseck@gmail.com>
2015-04-29 11:40:40 +0200

    Changed docker installation to include aufs, solving mount issues

--- docker-compose.sh
+++ docker-compose.sh
@@ -2,22 +2,16 @@
 
 #installs docker and docker-compose
 
-export DOCKER_HOST=tcp://localhost:4243
+sudo apt-get update && sudo apt-get install -qqy curl
 
 #installing docker
 if [ ! -x /usr/bin/docker ]; then
-	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-	sudo sh -c "echo deb https://get.docker.com/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-	sudo apt-get update
-	sudo apt-get -qqy install lxc-docker-1.5.0
+	curl -s https://get.docker.com/ | sh
 	sudo usermod -a -G docker `id -g -n` # requires relogin
 fi
 
 #Installing Compose
 if [ ! -x /usr/local/bin/docker-compose ]; then
-	if [ ! -x /usr/bin/curl ]; then
-		sudo apt-get install -qqy curl
-	fi
 	sudo bash -c "curl -sL https://raw.githubusercontent.com/docker/compose/1.1.0/contrib/completion/bash/docker-compose > /etc/bash_completion.d/docker-compose"
 	sudo bash -c "curl -sL https://github.com/docker/compose/releases/download/1.1.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 	sudo chmod +x /usr/local/bin/docker-compose
