471c8efc760d9bc0b22bf2ff43d791bd3fbfbd1d
Jonas Eckerström <jonaseck@gmail.com>
2015-05-07 15:49:21 +0200

    added apparmor

--- docker-compose.sh
+++ docker-compose.sh
@@ -2,9 +2,7 @@
 
 #installs docker and docker-compose
 
-if [ ! -x /usr/bin/cur ]; then
-	sudo apt-get update && sudo apt-get install -qqy curl
-fi
+sudo apt-get update && sudo apt-get install -qqy curl cgroup-lite apparmor
 
 #docker
 curl -s https://get.docker.com/ | sh
@@ -15,3 +13,4 @@ sudo bash -c "curl -sL https://raw.githubusercontent.com/docker/compose/1.2.0/co
 #use a forked compose until urls are supported again in 1.3.0
 sudo bash -c "curl -sL https://github.com/jonaseck2/compose/releases/download/1.2.0-urlfix/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
 sudo chmod +x /usr/local/bin/docker-compose
+	
